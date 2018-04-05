caf511432955c19e29c1d499f8ea5d5135a64545
Hieu Truong <hieu@tnhlabs.com>
2018-04-05 17:20:28 -0500

    Use drive controller

--- MoreArduinoController.ino
+++ MoreArduinoController.ino
@@ -1,29 +1,10 @@
 #include "BotMessage.h"
-#include "WifiProcessor.h"
+#include "DriveController.h"
 #include "UltrasonicProcessor.h"
+#include "WifiProcessor.h"
 #include <ArduinoJson.h>
 #include <SoftwareSerial.h>
 
-#define enA 5
-#define in1 4
-#define in2 3
-
-#define enB 6
-#define in3 7
-#define in4 8
-/*
-  Left motor is A
-  Right motor is B
-   _______
-   HIGH   | Backward
-   LOW    |
-   -------
-
-   _______
-   LOW   | Forward
-   HIGH  |
-   -------
-*/
 const int SerialBaudRate = 9600;
 
 const int WifiRxPin = 12;
@@ -38,13 +19,22 @@ const double MinimumDistance = 1;
 const double MaximumDistance = 50;
 const int YoyoSpeedMultiplier = 5;
 
-const int LedPin = 13;
+// Motor pins
+const int LeftSpeedPin = 5;
+const int LeftDirectionPin1 = 4;
+const int LeftDirectionPin2 = 3;
+const int RightSpeedPin = 6;
+const int RightDirectionPin1 = 7;
+const int RightDirectionPin2 = 8;
 
-WifiProcessor wifi(WifiRxPin, WifiTxPin);
-UltrasonicProcessor sonic(TriggerPin, EchoPin);
+const int LedPin = 13;
 
 int moveMode = 0;
 int moveSpeed = 50;
+
+WifiProcessor wifi(WifiRxPin, WifiTxPin);
+UltrasonicProcessor sonic(TriggerPin, EchoPin);
+DriveController driver(LeftSpeedPin, LeftDirectionPin1, LeftDirectionPin2, RightSpeedPin, RightDirectionPin1, RightDirectionPin2);
 
 void setup()
 {
@@ -52,21 +42,7 @@ void setup()
     wifi.begin("MORETechCo");
 
     sonic.setup();
-
-    pinMode(enA, OUTPUT);
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-
-    pinMode(enB, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
-
-    // Set initial rotation direction
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
+    driver.setup();
 
     pinMode(LedPin, OUTPUT);
     digitalWrite(LedPin, LOW);
@@ -98,107 +74,33 @@ void loop()
             double differenceToTarget = distance - TargetDistance;
             double absoluteDifference = abs(differenceToTarget);
             if (absoluteDifference >= MinimumDistance && absoluteDifference <= TargetDistance) {
-                forward(differenceToTarget * YoyoSpeedMultiplier);
+                if (differenceToTarget >= 0) {
+                    driver.goForward(absoluteDifference * YoyoSpeedMultiplier);
+                } else {
+                    driver.goBackward(absoluteDifference * YoyoSpeedMultiplier);
+                }
             }
         } else {
-            forward(0);
+            driver.goForward(0);
         }
     } else if (command != "") {
         if (data == "stop") {
             digitalWrite(LedPin, LOW);
-            forward(0);
+            driver.goForward(0);
         } else if (data == "forward") {
             digitalWrite(LedPin, HIGH);
-            forward(moveSpeed);
+            driver.goForward(moveSpeed);
         } else if (data == "backward") {
             digitalWrite(LedPin, HIGH);
-            backward(moveSpeed);
+            driver.goBackward(moveSpeed);
         } else if (data == "right") {
             digitalWrite(LedPin, HIGH);
-            right(moveSpeed);
+            driver.turnRight(moveSpeed);
         } else if (data == "left") {
             digitalWrite(LedPin, HIGH);
-            left(moveSpeed);
+            driver.turnLeft(moveSpeed);
         }
     }
 
     delay(10);
-}
-
-/*
-   Driving functions
-   Inputs: speed 0 - 100
-   Example calls:
-   left(100);
-   forward(23);
-   right(val);
-   backward(0);
-*/
-void right(int vel)
-{
-    vel = map(vel, 0, 100, 0, 255);
-    //Left forwards
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-
-    //Right backwards
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-
-    //Tell them how fast to go (vel -> speed)
-    analogWrite(enA, vel);
-    analogWrite(enB, vel);
-}
-
-void left(int vel)
-{
-    vel = map(vel, 0, 100, 0, 255);
-    //Left backwards
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-
-    //Right forwards
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-
-    //Tell them how fast to go (vel -> speed)
-    analogWrite(enA, vel);
-    analogWrite(enB, vel);
-}
-
-void forward(int vel)
-{
-    if (vel > 100)
-        vel = 100;
-    vel = map(vel, 0, 100, 0, 255);
-    if (vel > 0) {
-        //Left forwards
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-
-        //Right forwards
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-
-        //Tell them how fast to go (vel -> speed)
-        analogWrite(enA, vel);
-        analogWrite(enB, vel);
-    } else
-        backward(-vel);
-}
-
-void backward(int vel)
-{
-    vel = map(vel, 0, 100, 0, 255);
-    //Left backwards
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-
-    //Right backwards
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-
-    //Tell them how fast to go (vel -> speed)
-    analogWrite(enA, vel);
-    analogWrite(enB, vel);
 }

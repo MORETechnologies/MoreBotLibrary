765d8bef77fb3d2da63a9d270628dfcc773e30e4
Hieu Truong <hieu@tnhlabs.com>
2018-04-13 20:41:52 -0500

    Add setup from bot message method

--- MotorController.cpp
+++ MotorController.cpp
@@ -1,5 +1,13 @@
-#include "MotorController.h";
+#include "MotorController.h"
+#include "BotMessage.h"
 #include <Arduino.h>
+
+MotorController::MotorController()
+    : speedPin(0)
+    , directionPin1(0)
+    , directionPin2(0)
+{
+}
 
 MotorController::MotorController(int speedPin, int directionPin1, int directionPin2)
     : speedPin(speedPin)
@@ -13,6 +21,18 @@ void MotorController::setup()
     pinMode(speedPin, OUTPUT);
     pinMode(directionPin1, OUTPUT);
     pinMode(directionPin2, OUTPUT);
+}
+
+// indexes: speedPin = 0, directionPin1 = 1, directionPin2 = 2
+void MotorController::setup(BotMessage& botMessage)
+{
+    int pins[BotMessage::PinsArrayLength] = botMessage.getPins();
+
+    speedPin = pins[0];
+    directionPin1 = pins[1];
+    directionPin2 = pins[2];
+
+    setup();
 }
 
 void MotorController::rotateClockwise(int speed)
--- MotorController.h
+++ MotorController.h
@@ -1,13 +1,16 @@
 #ifndef MotorController_h
 #define MotorController_h
 
+#include "BotMessage.h"
 #include <Arduino.h>
 
 class MotorController
 {
 public:
+    MotorController();
     MotorController(int speedPin, int directionPin1, int directionPin2);
     void setup();
+    void setup(BotMessage& botMessage);
     void rotateClockwise(int speed);
     void rotateCounterclockwise(int speed);
 

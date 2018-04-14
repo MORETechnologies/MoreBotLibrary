59c192d4d7f796ab7c4ed3091d5128d35c0b07db
Hieu Truong <hieu@tnhlabs.com>
2018-04-13 21:02:12 -0500

    Add library header

---
+++ MoreLibrary.h
@@ -0,0 +1,11 @@
+#ifndef MoreLibrary_h
+#define MoreLibrary_h
+
+#include "BotMessage.h"
+#include "DriveController.h"
+#include "MoreBotRunner.h"
+#include "MotorController.h"
+#include "UltrasonicProcessor.h"
+#include "WifiProcessor.h"
+
+#endif
